b9ee628b8f0e0802ec67c3d15abef85c54827a66
Sujal Thapa1 <sujalmangar304@gmail.com>
2025-01-17 23:20:02 +0530

    Python code to print the largest number entered by user

---
+++ largest_num_check.py
@@ -0,0 +1,7 @@
+# checks the largest input number 
+
+first_num=int(input("Enter the first number"))
+sec_num=int(input("Enter the second number"))
+third_num=int(input("Enter the third number"))
+largest=max(first_num,sec_num,third_num)
+print(f"The largest number is {largest}")
